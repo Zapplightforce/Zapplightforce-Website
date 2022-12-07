95fa4df406a116fb3cafb2c50b38a1c082057a58
Zapplightforce <pers0010@hz.nl>
2022-12-07 22:47:22 +0100

    added info card

---
+++ resources/views/dashboard/infoCard.blade.php
@@ -0,0 +1,17 @@
+@extends('dashboard.dashboard')
+@section('mainBody')
+    <div class="min-h-screen ml-14 mt-14 mb-10 md:ml-64">
+
+        <!-- component -->
+        <!-- This is an example component -->
+        <div id="whoobe-3fery" class="w-full md:w-64 justify-center items-center bg-white shadow-lg rounded-lg flex flex-col">
+            <img src="https://res.cloudinary.com/moodgiver/image/upload/v1633344243/adventure_woman_rujic1.webp" alt="img" title="img" class="w-full h-auto object-cover rounded-t-lg" id="whoobe-ixxe5">
+            <div id="whoobe-1okdg" class="w-full p-4 justify-start flex flex-col">
+                <h4 class="border-b-2 text-3xl" id="whoobe-3mr7n">Info Card</h4>
+                <p class="my-4" id="whoobe-950fw">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ac tortor dignissim convallis aenean. Imperdiet massa tincidunt nunc pulvinar.</p>
+                <button value="button" class="my-4 px-4 py-2 text-white hover:bg-blue-700 bg-blue-500" id="whoobe-jkkr2">Read more</button>
+            </div>
+        </div>
+
+    </div>
+@endsection
